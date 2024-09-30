83e9d0741e16e3210f0f5ce617348066486db816
Jonas Urup Henriksen <Juhe20@student.aau.dk>
2024-09-30 12:29:38 +0200

    Added comments

--- Assets/Scripts/PythonReceiver.cs
+++ Assets/Scripts/PythonReceiver.cs
@@ -29,6 +29,7 @@ public class PythonReceiver : MonoBehaviour
 
     private void Update()
     {
+        //Changes position of each gameobject depending on the received position from Python.
         dog1.transform.position = blackposition1;
         dog2.transform.position = blackposition2;
         dog3.transform.position = blackposition3;
@@ -42,6 +43,7 @@ public class PythonReceiver : MonoBehaviour
         mThread.Start();
     }
 
+    //Network stuff for Python to work with Unity
     private void GetInfo()
     {
         localAdd = IPAddress.Parse(connectionIP);
@@ -63,13 +65,16 @@ public class PythonReceiver : MonoBehaviour
     {
         NetworkStream nwStream = client.GetStream();
         byte[] buffer = new byte[client.ReceiveBufferSize];
-
+        //Reads the message from the Python script
         int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
         string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead);
 
+        //Checks if the data read from above is a non empty string.
         if (!string.IsNullOrEmpty(dataReceived))
         {
             Debug.Log($"Received JSON: {dataReceived}");
+            //Uses a dictionary to check for key-value pairs from the json string.
+            //Enables the check of a string key to give the corresponding position.
             var receivedData = JsonConvert.DeserializeObject<Dictionary<string, List<float>>>(dataReceived);
 
             if (receivedData.ContainsKey("blackposition1"))
@@ -85,9 +90,8 @@ public class PythonReceiver : MonoBehaviour
                 whiteposition = new Vector3(receivedData["whiteposition1"][0], 0, receivedData["whiteposition1"][1]);
 
             print("Received data and updated the object!");
-            Debug.Log($"checking blackposition1 {blackposition1}");
 
-
+            //Sends data back to Python if necessary at a later point
             byte[] myWriteBuffer = Encoding.ASCII.GetBytes("Hey I got your message Python! Do You see this message?");
             nwStream.Write(myWriteBuffer, 0, myWriteBuffer.Length);
         }
